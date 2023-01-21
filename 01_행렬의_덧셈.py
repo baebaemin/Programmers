1993c01202fa9f844dd1f5b49a194066de0c5655
배민경 <baeminkyoung@baemingyeong-ui-MacBookPro.local>
2023-01-21 09:10:11 +0900

    23-01-21/09:10

--- 01_행렬의_덧셈.py
+++ 01_행렬의_덧셈.py
@@ -1,8 +1,14 @@
-def solution(arr1, arr2):
-    lst = arr1
-    for i in range(len(arr1)):
-        for j in range(len(arr1[i])):
-            lst[i][j] = arr1[i][j] + arr2[i][j]
-    return lst
+# def solution(arr1, arr2):
+#     lst = arr1
+#     for i in range(len(arr1)):
+#         for j in range(len(arr1[i])):
+#             lst[i][j] = arr1[i][j] + arr2[i][j]
+#     return lst
 
-print(solution([[1], [2]], [[3], [4]]))
+# print(solution([[1,2], [2,3]], [[3,4], [5,6]]))
+
+# zip()을 이용해 풀어보기
+def solution(arr1, arr2):
+    return [[sum(pair) for pair in zip(a, b)] for a, b in zip(arr1, arr2)]
+
+solution([[1, 2], [2, 3]], [[3, 4], [5, 6]])
