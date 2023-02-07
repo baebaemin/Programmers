8850b8652356642f9d08d01e73e6846f7a711401
배민경 <hellobaemin@gmail.com>
2023-02-07 23:58:59 +0900

    230207/23:58

--- 120866_안전지대.py
+++ 120866_안전지대.py
@@ -1,34 +1,36 @@
-board_lst = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0]]
-# 16
+board_lst = [[0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 1, 1, 0],
+             [0, 0, 0, 0, 0]]
 
 
 def solution(board):
-    # find index of 1 -> board[i][j]
+    # 8방향 검사를 위한 리스트 생성
     dr = [-1, -1, -1, 0, 0, 1, 1, 1]
-    dc = [-1, 0, 1, -1, 1, -1, 0, -1]
+    dc = [-1, 0, 1, -1, 1, -1, 0, 1]
 
-    for r in range(len(board)):
-        for c in range(len(board)):
+    N = len(board)
+    points = []  # 지뢰의 포인트를 담을 리스트 생성
+
+    for r in range(N):
+        for c in range(N):
             if board[r][c]:  # 지뢰가 묻혀있다면
-                break
+                points.append([r, c])  # 리스트에 추가
+                if len(points) > N * N - 8:  # 런타임 초과를 막기위한 조건
+                    return 0
 
-    for i in range(8):  # 8방으로 델타 검사
-        print(i, r + dr[i], c + dc[i]) ##
-        if 0 <= r + dr[i] <= len(board) and 0 <= c + dc[i] <= len(board):
-            board[r + dr[i]][c + dc[i]] = 1
-
-    print(board)
+    for p in points:  # 지뢰 개수만큼 반복
+        for i in range(8):  # 8방으로 델타 검사
+            idr, idc = p[0] + dr[i], p[1] + dc[i]
+            if 0 <= idr < N and 0 <= idc < N:  # 인덱스를 벗어나지 않는 조건으로
+                board[idr][idc] = 1  # board에 X표기
 
     cnt = 0
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if not board[r][c]:
-                cnt += 1
+    for r in range(N):
+        for c in range(N):
+            if not board[r][c]:  # 안전지대라면
+                cnt += 1  # cnt 누적
     return cnt
 
 print(solution(board_lst))
--- temp.py
+++ temp.py
@@ -1,2 +1,31 @@
-lst1 = [[]*2]
-print(lst1)
+board_lst = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+def solution(board):
+    dr = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dc = [-1, 0, 1, -1, 1, -1, 0, 1]
+    r_point = []
+    c_point = []
+
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c]:  # 지뢰가 묻혀있다면
+                r_point.apprnd(r)
+                c_point.append(c)
+
+    for j in range(len(r_point)):
+        for i in range(8):  # 8방으로 델타 검사
+            if 0 <= r_point[j] + dr[i] <= len(board) and 0 <= c_point[j] + dc[i] <= len(board):
+                board[r_point[j] + dr[i]][c_point[j] + dc[i]] = 1
+
+    cnt = 0
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if not board[r][c]:
+                cnt += 1
+    return cnt
